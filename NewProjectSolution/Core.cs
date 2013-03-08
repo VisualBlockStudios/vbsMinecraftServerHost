c5a44f4a5c975304a8a757246507a81fb3e3c610
VisualBlockStudios <leon.visualblockstudios@gmail.com>
2013-03-09 00:14:33 +0100

    Core Optimized #01

--- NewProjectSolution/Core.cs
+++ NewProjectSolution/Core.cs
@@ -5,13 +5,24 @@ using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Core.cs for VisualBlock Studios Minecraft Server Host
+ * Author: Leon K. ,
+ * 
+ * VisualBlock Studios
+ * Licensed under GNU v.3
+ */
 public class Core
 {
+    [STAThread]
     public int latestBuildNum;
 	public static Core()
 	{
         try{
             Application.Run(/*new Application*/);
+            if(!File.Exists(@"C:\Users\Public\Documents\VisualBlock\Error.txt")){
+                File.Move(@"C:\Users\Public\Documents\VisualBlock\Error.txt", @"C:\Users\Public\Documents\VisualBlock\Error\Error.txt");
+            }
         }
         catch (Exception ex){
             Error(ex, true);
@@ -34,7 +45,7 @@ public class Core
     {
         if (writeinFile == true)/*Write in a file located at C:\Users\Public\Documents\VisualBlock*/{
             TextWriter errorInFile = new TextWriter();
-            errorInFile.Write(ErrorMessage, @"C:\Users\Public\Documents\VisualBlock");
+            errorInFile.Write(ErrorMessage, @"C:\Users\Public\Documents\VisualBlock\Error.txt");
             errorInFile.Close();
         }
         else{
